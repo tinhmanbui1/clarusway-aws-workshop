214a51dcbd293f5b5a5bc09172f4187f4eaec61d
tinhmanbui1 <tinhmanbui@gmail.com>
2021-02-23 02:28:03 -0500

    added first aws project

--- projects/001-roman-numerals-converter/app.py
+++ projects/001-roman-numerals-converter/app.py
@@ -30,5 +30,5 @@ def main_post():
     return render_template('result.html', developer_name='Tinh', number_decimal=number, number_roman=convert(number))
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=80)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
+    # app.run(debug=True)
